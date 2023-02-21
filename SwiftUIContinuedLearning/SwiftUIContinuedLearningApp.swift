f1b94239a615bb3bb9731a57b6d7b9a7a54fcf6e
Baris Ciftci <barishciftci@gmail.com>
2023-02-21 19:42:11 +0100

    Initial Commit

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLearningApp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Baris Ciftci on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
