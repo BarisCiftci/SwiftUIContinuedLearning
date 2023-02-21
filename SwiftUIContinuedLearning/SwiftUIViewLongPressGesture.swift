718c2c5e206c89b04d439f91e0c8cf1ee449dd44
Baris Ciftci <barishciftci@gmail.com>
2023-02-21 20:15:41 +0100

    How to use onLongPressGesture

---
+++ SwiftUIContinuedLearning/SwiftUIViewLongPressGesture.swift
@@ -0,0 +1,87 @@
+//
+//  SwiftUIViewLongPressGesture.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Baris Ciftci on 21/02/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIViewLongPressGesture: View {
+    
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
+    
+    var body: some View {
+        VStack {
+            Rectangle()
+                .fill(isSuccess ? Color.green : Color.pink)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("CLICK HERE")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color.black)
+                    .cornerRadius(26)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 20) { (isPressing) in
+                        // Start of press -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                isComplete = true
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation(.easeInOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    } perform: {
+                        // at the min duration
+                        withAnimation(.easeInOut) {
+                            isSuccess = true
+                        }
+                    }
+
+                
+                Text("RESET")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color.black)
+                    .cornerRadius(26)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+            }
+        }
+        
+        
+        
+        
+//        Text(isComplete ? "COMPLETED" : " NOT COMPLETE" )
+//            .padding()
+//            .padding(.horizontal)
+//            .background(isComplete ? Color.green : Color.gray)
+//            .cornerRadius(10)
+//            .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) {
+//                isComplete.toggle()
+//            }
+    }
+}
+
+struct SwiftUIViewLongPressGesture_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewLongPressGesture()
+    }
+}
